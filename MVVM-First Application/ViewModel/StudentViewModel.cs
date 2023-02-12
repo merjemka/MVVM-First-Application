c170d214fea6f5d62af0e092433aac5ecdcbb7f5
PC <PC@DESKTOP-C2B4BDT>
2023-02-12 20:33:59 +0100

    Add project files.

---
+++ MVVM-First Application/ViewModel/StudentViewModel.cs
@@ -0,0 +1,29 @@
+﻿using MVVM_First_Application.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_First_Application.ViewModel
+{
+    public class StudentViewModel
+    {
+        public ObservableCollection<Student> Students
+        {
+            get;
+            set;
+        }
+
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+            students.Add(new Student { FirstName = "Merjem", LastName = "Kadunic" });
+            students.Add(new Student { FirstName = "Vigor", LastName = "Dundovic" });
+            students.Add(new Student { FirstName = "Hadzera", LastName = "Dzihana" });
+
+            Students = students;
+        }
+    }
+}
